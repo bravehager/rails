d7bee1fede40506bfec15c0f4a3b3be09cb397e5
Brave Hager <bravehager7@gmail.com>
2023-04-04 17:04:22 -0400

    Don't use physical processor count for determining available processors

--- activesupport/lib/active_support/test_case.rb
+++ activesupport/lib/active_support/test_case.rb
@@ -79,7 +79,7 @@ module ActiveSupport
       # number of tests to run is above the +threshold+ param. The default value is
       # 50, and it's configurable via +config.active_support.test_parallelization_threshold+.
       def parallelize(workers: :number_of_processors, with: :processes, threshold: ActiveSupport.test_parallelization_threshold)
-        workers = Concurrent.physical_processor_count if workers == :number_of_processors
+        workers = Concurrent.processor_count if workers == :number_of_processors
         workers = ENV["PARALLEL_WORKERS"].to_i if ENV["PARALLEL_WORKERS"]
 
         return if workers <= 1
